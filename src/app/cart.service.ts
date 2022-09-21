40960fd01d0d3b7d79c88afa602f018ee74cee9e
WannaBeStronger <101449341+WannaBeStronger@users.noreply.github.com>
2022-09-22 01:19:45 +0800

    4 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Product } from './products';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  items: Product[] = [];
 
-  constructor() { }
+  constructor() {}
+
+  // 将产品附加到 items 数组中
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  // 收集用户加到购物车中的商品，并返回每个商品及其数量
+  getItems() {
+    return this.items;
+  }
+
+  // 返回一个空数组
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
